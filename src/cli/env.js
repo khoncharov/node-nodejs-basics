01391bc043c8da527401d921c664f52539364bda
Konstantin Honcharov <k.honcharov@gmail.com>
2022-06-04 18:28:55 +0300

    feat: add env solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 import { env, stdout } from "process";
 
+env.RSS_testProp1 = "value101";
+env.RSS_testProp2 = "value102";
+
 export const parseEnv = () => {
   const resList = Object.entries(env).filter((prop) => /^RSS_.*/.test(prop[0]));
   let resStr = "";
