e0fbed6bc51fda09e7d7a6db0a0b9b552d8d40ea
Konstantin Honcharov <k.honcharov@gmail.com>
2022-06-04 15:41:38 +0300

    feat: add copy solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,36 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { mkdir, readdir, copyFile } from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const copy = async () => {
-    // Write your code here 
+  const sourceDir = "files";
+  const targetDir = "files_copy";
+
+  try {
+    // check if source folder exists
+    await readdir(join(__dirname, sourceDir));
+
+    // make files folder_copy with flag recursive false (default) (err if exists)
+    await mkdir(join(__dirname, targetDir), { recursive: false });
+
+    // make list of Dirent obj
+    const fileList = await readdir(join(__dirname, sourceDir), { withFileTypes: true });
+
+    // copy content
+    fileList.forEach(async (item) => {
+      if (item.isFile()) {
+        await copyFile(
+          join(__dirname, sourceDir, item.name),
+          join(__dirname, targetDir, item.name)
+        );
+      }
+    });
+  } catch (error) {
+    console.error("FS operation failed");
+  }
 };
+
+copy();
