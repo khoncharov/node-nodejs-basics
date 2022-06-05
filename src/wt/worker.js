48fa30e837b2f8ef84dfde96065cf15929ba56e5
Konstantin Honcharov <k.honcharov@gmail.com>
2022-06-05 17:21:41 +0300

    feat: add worker solution

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,17 @@
+import { workerData, parentPort } from "worker_threads";
+
 // n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // This function sends result of nthFibonacci computations to main thread
+  // Err case test:
+  // if (workerData === 11) {
+  //   throw new Error();
+  // }
+  const result = nthFibonacci(workerData);
+  parentPort.postMessage(result);
 };
+
+sendResult();
