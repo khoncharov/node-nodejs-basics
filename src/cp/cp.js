a87cde45911ac458ccfec357059ddafa7edff7c2
Konstantin Honcharov <k.honcharov@gmail.com>
2022-06-05 23:01:01 +0300

    feat: add child process solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,43 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { spawn } from "child_process";
+import { stdin } from "process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptFile = "script.js";
+  const options = {
+    cwd: join(__dirname, "./files"),
+    stdio: ["pipe", "pipe", "pipe", "ipc"],
+  };
+  const cp = spawn(process.argv[0], [scriptFile, ...args], options);
+
+  console.log("ipc connected >", cp.connected, "\n");
+
+  stdin.on("data", (data) => {
+    cp.send(data);
+  });
+
+  cp.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  cp.stderr.on("data", (data) => {
+    console.error(data.toString());
+  });
+
+  cp.on("exit", (code) => {
+    console.log(`Child exited with code ${code}`);
+    process.exit(0);
+  });
 };
+
+// Read args from cli
+// const args = process.argv.slice(2);
+
+// Read args from variable
+const args = ["--arg1", "--arg2"];
+
+spawnChildProcess(args);
--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -4,9 +4,11 @@ console.log(`Total number of arguments is ${args.length}`);
 console.log(`Arguments: ${JSON.stringify(args)}`);
 
 const echoInput = (chunk) => {
-    const chunkStringified = chunk.toString();
-    if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+  const chunkStringified = Buffer.from(chunk.data).toString();
+  if (chunkStringified.includes("CLOSE")) {
+    process.exit(0);
+  }
+  process.stdout.write(`Received from master process: ${chunkStringified}\n`);
 };
 
-process.stdin.on('data', echoInput);
+process.on("message", echoInput);
